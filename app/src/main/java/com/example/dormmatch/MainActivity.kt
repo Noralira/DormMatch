953d086393101cca17bcbc4a0edaefd950504d24
Vivian Martins <64610367+vivianmartins@users.noreply.github.com>
2023-04-07 16:46:24 +0100

    TesteBtn

--- app/src/main/java/com/example/dormmatch/MainActivity.kt
+++ app/src/main/java/com/example/dormmatch/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.dormmatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registo_activity)
+        setContentView(R.layout.login_activity)
     }
+
+    fun btnLogin(view: View) {   setContentView(R.layout.home_user)}
 }
