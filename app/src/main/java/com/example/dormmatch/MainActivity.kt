6ae16e6f96e7b69802f4f7c8959eaf8073a0a19a
Rita Guimarães <kikasgui@gmail.com>
2023-04-17 12:04:18 +0100

    update login

--- app/src/main/java/com/example/dormmatch/MainActivity.kt
+++ app/src/main/java/com/example/dormmatch/MainActivity.kt
@@ -2,20 +2,12 @@ package com.example.dormmatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_login)
+
     }
 
-    fun btnLogin(view: View) {   setContentView(R.layout.home_user)}
-    fun btnRegister(view: View) {
-        setContentView(R.layout.registo_activity)
-    }
-
-    fun btnVoltar(view: View) {
-        setContentView(R.layout.login_activity)
-    }
 }
