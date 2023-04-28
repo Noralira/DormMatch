4d25edf919071a62f81571875158fb11024acde2
Shnip1906 <silva.pedro@ipvc.pt>
2023-04-28 14:04:05 +0100

    Navbar and some adjustments

---
+++ app/src/main/java/com/example/dormmatch/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package com.example.dormmatch
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dormmatch.databinding.RegistoActivityBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: RegistoActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RegistoActivityBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+    fun btnVoltar(view: View) {
+        setContentView(R.layout.login_activity)
+    }
+}
