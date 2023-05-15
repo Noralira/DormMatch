92a9e905a80346cea6a4379bb3398486136613a3
Shnip1906 <silva.pedro@ipvc.pt>
2023-05-15 10:21:18 +0100

    Fix problem

--- app/src/main/java/com/example/dormmatch/RegisterActivity.kt
+++ app/src/main/java/com/example/dormmatch/RegisterActivity.kt
@@ -5,14 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.dormmatch.databinding.RegistoActivityBinding
+import com.example.dormmatch.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding: RegistoActivityBinding
+    private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = RegistoActivityBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
     }
