c7a11a00e4495f8f14dedc12188063b7e2d33818
Rita Guimarães <kikasgui@gmail.com>
2023-05-16 20:18:32 +0100

    update register files

--- app/src/main/java/com/example/dormmatch/RegisterActivity.kt
+++ app/src/main/java/com/example/dormmatch/RegisterActivity.kt
@@ -5,24 +5,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.dormmatch.databinding.RegistoActivityBinding
+import com.example.dormmatch.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding: RegistoActivityBinding
+    private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = RegistoActivityBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
     }
 
     fun btnReturn(view: View) {
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_register)
     }
 
     fun btnCreateAccount(view: View) {
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_register)
 
         // notificar se conta for criada com sucesso
     }
