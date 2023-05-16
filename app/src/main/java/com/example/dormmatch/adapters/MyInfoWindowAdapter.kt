43749215b3648bd60d41a57d95f0c0f615ece259
Shnip1906 <silva.pedro@ipvc.pt>
2023-05-16 15:09:42 +0100

    Fix image bug

--- app/src/main/java/com/example/dormmatch/adapters/MyInfoWindowAdapter.kt
+++ app/src/main/java/com/example/dormmatch/adapters/MyInfoWindowAdapter.kt
@@ -1,11 +1,9 @@
 package com.example.dormmatch.adapters
 
-import android.content.ContentValues.TAG
 import android.view.View
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
 import com.google.android.gms.maps.model.Marker
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,16 +13,14 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
-import java.net.URL
 
-class MyInfoWindowAdapter(val context: Context, val idPropriedade: ArrayList<String>): InfoWindowAdapter {
+class MyInfoWindowAdapter(private val context: Context): InfoWindowAdapter {
 
     override fun getInfoContents(p0: Marker): View? {
         TODO("Not yet implemented")
     }
 
     override fun getInfoWindow(p0: Marker): View {
-
         val infoView: View = LayoutInflater.from(context).inflate(R.layout.custom_info, null)
 
         val title: TextView = infoView.findViewById(R.id.title_info)
@@ -33,14 +29,11 @@ class MyInfoWindowAdapter(val context: Context, val idPropriedade: ArrayList<Str
 
         val URLimage = "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/featured-image-bedroom-decor.jpeg.jpg"
 
-        Log.d(TAG, idPropriedade.toString())
-
-        val delim = "_"
-        var arraySnippet = p0.snippet.toString().split(delim).toTypedArray()
+        val delim = "&_:_&"
+        val arraySnippet = p0.snippet.toString().split(delim).toTypedArray()
         title.setText(arraySnippet[0])
         desc.setText(arraySnippet[1])
         Picasso.get().load(p0.title).into(imageView)
-
 
         return infoView
 
--- app/src/main/java/com/example/dormmatch/fragments/Maps.kt
+++ app/src/main/java/com/example/dormmatch/fragments/Maps.kt
@@ -1,32 +1,25 @@
 package com.example.dormmatch.fragments
 
 import android.content.ContentValues.TAG
-import android.icu.text.Transliterator.Position
 import android.location.Geocoder
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebSettings.ZoomDensity
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBindings
 import com.example.dormmatch.R
 import com.example.dormmatch.adapters.MyInfoWindowAdapter
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.squareup.picasso.Picasso
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,10 +28,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Map.newInstance] factory method to
+ * Use the [Maps.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Map : Fragment(), OnMapReadyCallback {
+class Maps : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -112,7 +105,7 @@ class Map : Fragment(), OnMapReadyCallback {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Map().apply {
+            Maps().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -126,14 +119,14 @@ class Map : Fragment(), OnMapReadyCallback {
 
         val address = "Avenida do Atlântico Viana do Castelo"
 
-        googleMap.setInfoWindowAdapter(MyInfoWindowAdapter(requireContext(), imagem))
+        googleMap.setInfoWindowAdapter(MyInfoWindowAdapter(requireContext()))
 
         // ZOOM AND MOVE CAMERA NA LOCALIZAÇÃO ATUAL
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(getCoord(address)))
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(getCoord(address), 18.0f))
 
-        for (i in 0 until localizacao.size) {
-            googleMap.addMarker(MarkerOptions().position(getCoord(localizacao[i])).title(imagem[i]).snippet(titulo[i] + '_' + descricao[i]))
+        for (i in 0 until idPropriedade.size) {
+                googleMap.addMarker(MarkerOptions().position(getCoord(localizacao[i])).title(imagem[i]).snippet(titulo[i] + "&_:_&" + descricao[i]))
         }
 
     }
@@ -157,11 +150,13 @@ class Map : Fragment(), OnMapReadyCallback {
                             val idP = "${snap.child("idPropriedade").value}"
                             val desc = "${snap.child("descricao").value}"
                             val title = "${snap.child("titulo").value}"
+                            val Image = "${snap.child("imagemCapa").value}"
 
                             localizacao.add(loc)
                             descricao.add(desc)
                             titulo.add(title)
                             idPropriedade.add(idP)
+                            imagem.add(Image)
                         }
                     }
                 }
@@ -170,36 +165,5 @@ class Map : Fragment(), OnMapReadyCallback {
 
                 }
             })
-
-        // atraves do codigo do anuncio vou buscar uma imagem a BD
-        val ref2 = FirebaseDatabase.getInstance().getReference("foto")
-        ref2.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()) {
-                    var conta = 0
-                    for (anuncioSnap in snapshot.children) {
-                        val idP = "${anuncioSnap.child("idPropriedade").value}"
-                        for(idProp in idPropriedade){
-                            if (idP.equals(idProp)) {
-                                val image = "${anuncioSnap.child("imageData").value}"
-                                imagem.add(image)
-                                conta += 1
-                            }
-                        }
-
-                    }
-                    if (conta == 0) {
-                        val image = "https://wallpapers.com/images/featured-full/blank-white-7sn5o1woonmklx1h.jpg"
-                        imagem.add(image)
-                    }
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
-
-
     }
 }
