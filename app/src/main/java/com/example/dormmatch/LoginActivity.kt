7dd59e068bbbcb67e560efe5f249544603ebd5df
Rita Guimarães <kikasgui@gmail.com>
2023-05-16 20:24:21 +0100

    updated files

--- app/src/main/java/com/example/dormmatch/LoginActivity.kt
+++ app/src/main/java/com/example/dormmatch/LoginActivity.kt
@@ -1,18 +1,35 @@
 package com.example.dormmatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.dormmatch.databinding.ActivityLoginBinding
+import com.google.firebase.FirebaseApp
 
 
 class LoginActivity : AppCompatActivity() {
+    private val menuActivityRequestCode = 1
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-    }
+        binding = ActivityLoginBinding.inflate(layoutInflater)
 
-    fun btnLogin(view: View) {   setContentView(R.layout.home_user)}
-    fun btnRegister(view: View) { setContentView(R.layout.registo_activity)}
+        setContentView(binding.root)
+
+        FirebaseApp.initializeApp(this)
+
+        //setContentView(R.layout.login_activity)
+    }
+    fun btnLogin(view: View) {
+        //setContentView(R.layout.activity_main)
+        startActivityForResult(Intent(this, MenuActivity::class.java), menuActivityRequestCode)
+    }
+    fun btnRegister(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
 
 }
 
