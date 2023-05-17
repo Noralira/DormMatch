94c6491d25f504802da889ba6d52868b5dd27ef9
Rita Guimarães <kikasgui@gmail.com>
2023-05-17 04:18:50 +0100

    added connection to db & updated xml

--- app/src/main/java/com/example/dormmatch/LoginActivity.kt
+++ app/src/main/java/com/example/dormmatch/LoginActivity.kt
@@ -1,14 +1,18 @@
 package com.example.dormmatch
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import com.example.dormmatch.databinding.ActivityLoginBinding
 import com.google.firebase.FirebaseApp
-import com.example.dormmatch.databinding.MenuBottomNavbarBinding
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.FirebaseUser
+
 
 
 class LoginActivity : AppCompatActivity() {
@@ -25,24 +29,41 @@ class LoginActivity : AppCompatActivity() {
 
         //setContentView(R.layout.login_activity)
     }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
     fun btnLogin(view: View) {
-        //setContentView(R.layout.activity_main)
-        checkLogin()
-        startActivityForResult(Intent(this, MenuActivity::class.java), menuActivityRequestCode)
+        val email = findViewById<EditText>(R.id.et_email).text.toString();
+        val password = findViewById<EditText>(R.id.et_password).text.toString();
+
+        signIn(email, password)
     }
     fun btnRegister(view: View) {
         val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
     }
 
-    private fun checkLogin(view: View) {
-        val username = findViewById<EditText>(R.id.et_username).text.toString();
-        val password = findViewById<EditText>(R.id.et_password).text.toString();
-
-        getUsersDB()
-    }
-
-    fun getUsersDB() {
-        val ref = FirebaseDatabase.getInstance().getReference("user")
+    private fun updateUI(user: FirebaseUser?) {
     }
 }
