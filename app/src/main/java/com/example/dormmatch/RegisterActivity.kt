a4e82a8515d8dae45a0173446749c0191289da35
Rita Guimarães <kikasgui@gmail.com>
2023-05-17 03:21:26 +0100

    added connection to database

--- app/src/main/java/com/example/dormmatch/RegisterActivity.kt
+++ app/src/main/java/com/example/dormmatch/RegisterActivity.kt
@@ -1,22 +1,74 @@
 package com.example.dormmatch
 
 
-import android.content.Intent
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.dormmatch.databinding.ActivityLoginBinding
 import com.example.dormmatch.databinding.ActivityRegisterBinding
-import com.example.dormmatch.databinding.MenuBottomNavbarBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    private fun reload() {
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 
     fun btnReturn(view: View) {
@@ -24,8 +76,11 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     fun btnCreateAccount(view: View) {
-        //setContentView(R.layout.login_activity)
 
-        // notificar se conta for criada com sucesso
+        val email = findViewById<EditText>(R.id.et_email).text.toString()
+        val password = findViewById<EditText>(R.id.et_password).text.toString()
+
+        createAccount(email, password)
     }
+
 }
